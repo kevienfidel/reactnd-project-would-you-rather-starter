6acf321b9492db0a06c012ddaea2b87f6dbd1744
Kevien A. Fidel <kfidel@microsourcing.local>
2018-05-25 14:31:35 +0800

    reverted

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,50 +1,28 @@
 import React, {Component} from 'react'
-import {NavLink} from 'react-router-dom'
 import {connect} from 'react-redux'
-import {Nav, NavItem} from 'react-bootstrap'
 
-class Navigation extends Component {
+class Nav extends Component {
     render() {
-        // return (
-        //     <nav className='nav'>
-        //         <ul>
-        //             <li>
-        //                 <NavLink to={`/home/authedUser/${this.props.authedUser}`} exact activeClassName='active'>
-        //                     Home
-        //                 </NavLink>
-        //             </li>
-        //             <li>
-        //                 <NavLink to='/add' exact activeClassName='active'>
-        //                     New Poll
-        //                 </NavLink>
-        //             </li>
-        //             <li>
-        //                 <NavLink to='/leaderboard' exact activeClassName='active'>
-        //                     Leaderboard
-        //                 </NavLink>
-        //             </li>
-        //         </ul>
-        //     </nav>
-        // )
-
         return (
-            <Nav>
-                <NavItem>
-                    <NavLink to={`/home/authedUser/${this.props.authedUser}`} exact activeClassName='active'>
-                        Home
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink to='/add' exact activeClassName='active'>
-                        New Poll
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink to='/leaderboard' exact activeClassName='active'>
-                        Leaderboard
-                    </NavLink>
-                </NavItem>
-            </Nav>
+            <nav className='nav'>
+                <ul>
+                    <li>
+                        <NavLink to={`/home/authedUser/${this.props.authedUser}`} exact activeClassName='active'>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/add' exact activeClassName='active'>
+                            New Poll
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/leaderboard' exact activeClassName='active'>
+                            Leaderboard
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
         )
     }
 }
@@ -55,4 +33,4 @@ function mapStateToProps({authedUser}) {
     }
 }
 
-export default connect(mapStateToProps)(Navigation)
+export default connect(mapStateToProps)(Nav)
